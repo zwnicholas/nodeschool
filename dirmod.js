e8f0ec598eb59e2713523fc069732613c013c3a4
Zach Nicholas <zwnich@gmail.com>
2016-08-04 09:03:21 -0500

    cleaned up formatting

--- dirmod.js
+++ dirmod.js
@@ -4,17 +4,15 @@ var path = require('path');
 
  function daddyfunc(dirname, ext, callback) {
 	extapp = '.' + ext;
-	fs.readdir(dirname, function (err, list) { // list is the raw file list from readdir
-		
+	fs.readdir(dirname, function (err, list) { // list is the raw file list from readdir	
 		if (err)
 			return callback(err);// early return 
 		var	filtered = list.filter(function (p) {
-				return path.extname(p) === extapp
+			return path.extname(p) === extapp
 		});/*Key error was here. callback can't be in filter callback.
 		Need to find out why. I guess it would be preferable
 		to call the callback with the complete filtered array.
-		
-			*/
+			*/	
 		 callback(null, filtered);
 	});
 };
